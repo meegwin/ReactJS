4cf4bfbcc9b160760dfaa68deab1704e40388310
meegwin <trannhatquang27@gmail.com>
2020-09-27 19:17:05 +0700

    React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-import StateLessComponets from './Components/StateLessComponents';
-import StateFull, {StateFullComponents} from './Components/StateFullComponents';
+// import StateLessComponets from './Components/StateLessComponents';
+// import StateFull, {StateFullComponents} from './Components/StateFullComponents';
+import Home from './btComponents/Home';
 function App() {
   return (
       <div>
-        <StateLessComponets/>
-        <StateFull/>
+        <Home/>
+        {/* <StateLessComponets/>
+        <StateFull/> */}
       </div>
   );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// css
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//js
+import '../node_modules/jquery/dist/jquery.min';
+import '../node_modules/popper.js/dist/popper.min';
 ReactDOM.render(
   //noi dung in ra html
   <React.StrictMode>
